c00142eb8ab4724842d865992fd9bb0bc742a30c
Chris Symons <chris.symons@moodys.com>
2022-05-26 18:30:10 +0000

    Removed conflicting variable

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,6 @@ module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket = "terraform-workshop-s3-bucket"
   bucket_prefix = "chris-symons"
   acl    = "private"
 
